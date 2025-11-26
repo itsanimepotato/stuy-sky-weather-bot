ce0bd9fd84c1b006e036c1b71612cc86918ba39d
Ethan <echeung60@stuy.edu>
2025-11-26 11:41:34 -0500

    hehe didnt work in class

--- README.md
+++ README.md
@@ -3,4 +3,17 @@
 The purpose of this bot is to output weather data for the StuySky Discord server using a weather API and discord.py
 
 ## Commands:
-``` !date_time ``` = outputs current date and time
+
+```!info``` outputs this command list  
+
+### NO API COMMANDS:  
+``` !date_time ``` outputs current date and time  
+```!logo``` outputs the StuySky Logo
+```!spam N``` outputs a message of a bot counting up to N
+
+### METEOROLOGY COMMANDS:
+```!cstuy or !currentstuyweather``` outputs the weather at Stuyvesant High School  
+```!cpoint LATITUDE LONGITUDE``` outputs the weather at LATITUDE, LONGITUDE  
+
+### ASTRONOMY COMMANDS:
+```!apod``` outputs the Astronomy Picture Of the Day
--- bot.py
+++ bot.py
@@ -2,17 +2,16 @@
 
 import requests
 import json
-from discord.ext import commands, tasks
 import discord
+from discord.ext import commands, tasks
 from dataclasses import dataclass
 
 import datetime
 
-import nasapy
 import os
 import random
 import apodBackup
-
+import urllib.request
 
 ## DISCORD SIDE
 
@@ -30,15 +29,15 @@ bot = commands.Bot(command_prefix="!", intents=intents, activity=activity, statu
 
 # Commands with no API
 
-@bot.command() 
+@bot.command()
 async def info(ctx):
     _ = "`!_` \n"
     infoinfo = "`!info gives this list, the list of commands` \n"
     datetimeinfo = "`!date_time gives the current date and time` \n"
     currentstuyweatherinfo = "`!cstuy or !currentstuyweather gives the weather at Stuyvesant High School` \n"
     pointforecastinfo = "`!cpoint LATITUDE LONGITUDE gives the weather at LATITUDE, LONGITUDE, put lat and lon with no additions like !pointforecast 40.7127 -74.0061` \n"
-    apodinfo = "`!apod gives the astronomy picture of the day (currently unavailable due to gov shutdown)` \n"    
-    
+    apodinfo = "`!apod gives the astronomy picture of the day` \n"
+
     await ctx.send(f"{infoinfo}\n{datetimeinfo}\n{currentstuyweatherinfo}\n{pointforecastinfo}\n{apodinfo}")
 
 @bot.command()
@@ -47,28 +46,28 @@ async def logo(ctx):
 
 #gives current date and time
 @bot.command()
-async def date_time(ctx): 
+async def date_time(ctx):
     date = datetime.datetime.now().strftime("%x")
     print("date is " + date)
-    
+
     time = datetime.datetime.now().strftime("%H:%M:%S")
     print("time is " + time)
-    
+
     await ctx.send(f"The date is {date} and the time is {time}.")
-    
+
 @bot.command()
 async def spam(ctx, amt):
     n = int(amt)
     if n > 5:
         await ctx.send("i cant count that high sorry")
         return
-    
+
     for i in range(-1,n):
         if i == -1:
             await ctx.send(f"i am counting to {n}")
         else:
             await ctx.send(f"{i + 1}")
-    
+
 
 # Commands using the Weather API
 
@@ -76,12 +75,12 @@ async def spam(ctx, amt):
 
 #put this for commands needing the weather API
 
-''' 
+'''
 endpoint = 'https://api.weather.gov/INSERT_ENDPOINT_HERE'
 response = requests.get(endpoint, headers = headers)
 data = response.json()
 
-print(data) 
+print(data)
 
 '''
 
@@ -94,7 +93,7 @@ STUY_LON = -74.0061
 #gives the weather at point lat, lon
 @bot.command()
 async def cpoint(ctx, lat, lon):
-    
+
     try:
         #gets data at lat and lon coordinates
         endpoint = f'https://api.weather.gov/points/{lat},{lon}'
@@ -103,61 +102,61 @@ async def cpoint(ctx, lat, lon):
         print(endpoint)
         response = requests.get(endpoint, headers = headers)
         data = response.json()
-        
-        
+
+
         #gets all links about at pt (hourly daily etc)
         links = data["forecastGridData"]
         print(links)
-        
+
         #gets link relating to daily forecast at pt
         forecast_link = data["properties"]["forecast"]
         print(forecast_link)
-        
+
         #retrieves forecast data at coordinates
         forecast_response = requests.get(forecast_link, headers = headers)
         forecast_data = forecast_response.json()
         print(forecast_data)
-        
+
         #gets most recently updated forecast for next period (for example: Monday, Monday Night, Tuesday, etc.)
         forecast = forecast_data["properties"]["periods"][0]
-        
+
         timeOfDay = forecast["name"]
         date = datetime.datetime.now().strftime("%x")
         temp = forecast["temperature"]
         tempUnit = forecast["temperatureUnit"]
         windSpeed = forecast["windSpeed"]
         windDir = forecast["windDirection"]
-        await ctx.send(f'The following information is about {timeOfDay} on {date} at {lat} latitude and {lon} longitude. The temperature is {temp}{tempUnit}. The wind speed is {windSpeed} coming from {windDir}.')   
+        await ctx.send(f'The following information is about {timeOfDay} on {date} at {lat} latitude and {lon} longitude. The temperature is {temp}{tempUnit}. The wind speed is {windSpeed} coming from {windDir}.')
 
     except:
         await ctx.send(f"Sorry, we don't have data for {lat}° latitude and {lon}° longitude right now.")
 
 #gives the weather at stuy
 @bot.command()
-async def cstuy(ctx):   
+async def cstuy(ctx):
     try:
         #gets data at stuy coordinates
         endpoint = f'https://api.weather.gov/points/{STUY_LAT},{STUY_LON}'
         print(endpoint)
         response = requests.get(endpoint, headers = headers)
         data = response.json()
-        
+
         #gets all links about at stuy (hourly daily etc)
         links = data["properties"]
         print(links)
-        
+
         #gets link relating to daily forecast at stuy
         forecast_link = data["properties"]["forecast"]
         print(forecast_link)
-        
+
         #retrieves forecast data at stuy coordinates
         forecast_response = requests.get(forecast_link, headers = headers)
         forecast_data = forecast_response.json()
         print(forecast_data)
-        
+
         #gets most recently updated forecast for next period (for example: Monday, Monday Night, Tuesday, etc.)
         forecast = forecast_data["properties"]["periods"][0]
-        
+
         timeOfDay = forecast["name"]
         date = datetime.datetime.now().strftime("%x")
         temp = forecast["temperature"]
@@ -165,8 +164,8 @@ async def cstuy(ctx):
         windSpeed = forecast["windSpeed"]
         windDir = forecast["windDirection"]
         shortForecast = forecast["shortForecast"]
-        
-        await ctx.send(f'The following information is about {timeOfDay} on {date} at Stuy. The temperature is {temp}{tempUnit}. The wind speed is {windSpeed} coming from {windDir}. It should be {shortForecast}.')   
+
+        await ctx.send(f'The following information is about {timeOfDay} on {date} at Stuy. The temperature is {temp}{tempUnit}. The wind speed is {windSpeed} coming from {windDir}. It should be {shortForecast}.')
     except:
         await ctx.send(f"Sorry, we don't have data at Stuy right now.")
 
@@ -179,23 +178,23 @@ async def currentstuyweather(ctx):
         print(endpoint)
         response = requests.get(endpoint, headers = headers)
         data = response.json()
-        
+
         #gets all links about at stuy (hourly daily etc)
         links = data["properties"]
         print(links)
-        
+
         #gets link relating to daily forecast at stuy
         forecast_link = data["properties"]["forecast"]
         print(forecast_link)
-        
+
         #retrieves forecast data at stuy coordinates
         forecast_response = requests.get(forecast_link, headers = headers)
         forecast_data = forecast_response.json()
         print(forecast_data)
-        
+
         #gets most recently updated forecast for next period (for example: Monday, Monday Night, Tuesday, etc.)
         forecast = forecast_data["properties"]["periods"][0]
-        
+
         timeOfDay = forecast["name"]
         date = datetime.datetime.now().strftime("%x")
         temp = forecast["temperature"]
@@ -203,8 +202,8 @@ async def currentstuyweather(ctx):
         windSpeed = forecast["windSpeed"]
         windDir = forecast["windDirection"]
         shortForecast = forecast["shortForecast"]
-        
-        await ctx.send(f'The following information is about {timeOfDay} on {date} at Stuy. The temperature is {temp}{tempUnit}. The wind speed is {windSpeed} coming from {windDir}. It should be {shortForecast}.')   
+
+        await ctx.send(f'The following information is about {timeOfDay} on {date} at Stuy. The temperature is {temp}{tempUnit}. The wind speed is {windSpeed} coming from {windDir}. It should be {shortForecast}.')
     except:
         await ctx.send(f"Sorry, we don't have data at Stuy right now.")
 
@@ -213,27 +212,32 @@ async def currentstuyweather(ctx):
 
 ## NASA API SIDE
 NASA_KEY = "vwwlK5uqo0BRhb4H0J6KRWgGailqnGK93lC5cdDT"
-nasa = nasapy.Nasa(key=NASA_KEY)
 
 #gives astronomy picture of the day
 @bot.command()
 async def apod(ctx):
     try:
-        d = datetime.today().strftime('%Y-%m-%d')
-        print(d)
-        apod = nasa.picture_of_the_day(date=d, hd=True)
-        print(apod)
-        apod_json = apod.json()
-        print(apod_json)
-        image_url = apod_json['url']
-        print(image_url)
-        await ctx.send(image_url)
+        with urllib.request.urlopen(f'https://api.nasa.gov/planetary/apod?api_key={NASA_KEY}') as response:
+            response = response.read().decode()
+            print(response)
+            json_obj = json.loads(response)
+            print(json_obj['url'])
+            img = json_obj['url']
+            desc = json_obj['explanation']
+
+        n = 2
+        for i in range(0,n):
+            if i == 0:
+                await ctx.send(img)
+            else:
+                await ctx.send(f"Here is a description of the image above: \n{desc}")
+
     except:
         basepath = './apodBackup'
         print(os.listdir(basepath))
         image = random.choice(os.listdir(basepath))
         print(image)
-        
+
         n = 2
         for i in range(0,n):
             if i == 0:
